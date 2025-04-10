9204a613f8576a1c57ce1f09a61760387bc2773d
refrigerador67 <96502023+refrigerador67@users.noreply.github.com>
2025-04-09 23:05:22 -0300

    Trying to get the camera to work

--- app/src/main/java/com/refrigerador67/poslaroid/ui/CameraManager.kt
+++
@@ -1,2 +0,0 @@
-package com.refrigerador67.poslaroid.ui
-
--- app/src/main/java/com/refrigerador67/poslaroid/ui/MainActivity.kt
+++ app/src/main/java/com/refrigerador67/poslaroid/ui/MainActivity.kt
@@ -3,20 +3,43 @@ package com.refrigerador67.poslaroid.ui
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.refrigerador67.poslaroid.R
+import com.refrigerador67.poslaroid.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         if(!checkPerms()){
             ActivityCompat.requestPermissions(
                 this, RequiredPerms, 0
             )
+            startCamera()
+        }else{
+            startCamera()
         }
+
+
     }
 
     private fun checkPerms(): Boolean {
@@ -27,9 +50,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun startCamera(){
+        val processCameraProvider = ProcessCameraProvider.getInstance(this)
+        processCameraProvider.addListener({
+            val cameraProvider: ProcessCameraProvider = processCameraProvider.get()
+            val previewUseCase = Preview.Builder().build().also { it.setSurfaceProvider (binding.viewFinder.surfaceProvider) }
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
     companion object {
         private val RequiredPerms = arrayOf(
             Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
             Manifest.permission.BLUETOOTH,
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION,
