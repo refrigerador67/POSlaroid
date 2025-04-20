3d86d32703a7af633c6490583aabadc601407e6d
refrigerador67 <96502023+refrigerador67@users.noreply.github.com>
2025-04-20 17:44:48 -0300

    Updated version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
