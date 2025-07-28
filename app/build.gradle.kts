ffb8b4a1152fb8b337e6f9e2f8c34531ef82386b
refrigerador67 <96502023+refrigerador67@users.noreply.github.com>
2025-07-28 09:39:23 -0300

    Fixed Web Printing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4"
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
